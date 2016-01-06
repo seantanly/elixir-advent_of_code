ba6db2ffa90c3a9c34b5aad700fb361bcc3c03b8
Sean Tan <sean.tan.ly@gmail.com>
2016-01-07 00:44:21 +0800

    Added day 20 part 1: Using sqrt/1 to find divisors, it is way faster than testing 1..n using only rem/2

--- 20/part1.exs
+++ 20/part1.exs
@@ -1,10 +1,22 @@
+# Referenced solution provided on https://www.reddit.com/r/adventofcode/comments/3xjpp2/day_20_solutions/cy5ndio
+# about using sqrt to reduce the problem space.
 defmodule M do
 
-  def house_presents(house_num) do
-    1..house_num |>
-    Enum.reduce(0, fn elve_num, acc ->
-      if rem(house_num, elve_num) != 0, do: acc, else: elve_num * 10 + acc
+  def divisors(n) do
+    1..(n |> :math.sqrt |> trunc)
+    |> Enum.reduce([], fn
+      x, acc when n == x -> [x | acc]
+      x, acc when n == x * x -> [x | acc]
+      x, acc when rem(n, x) == 0 -> [div(n, x) | [x | acc]]
+      _ , acc -> acc
     end)
+  end
+
+  def house_presents(house_num) do
+    house_num
+    |> divisors
+    |> Enum.sum
+    |> :erlang.*(10)
   end
 
   def min_house(presents), do: min_house(presents, 1)
@@ -22,12 +34,14 @@ result = 1..9
 
 ^result = [10, 30, 40, 70, 60, 120, 80, 150, 130]
 
-# Simple brute takes 9 secs
+# Simple brute testing all 1..n without sqrt takes 9 secs, this takes 0.6 secs
 result = M.min_house(500_000)
 |> IO.inspect
 
 ^result = 13860
-# result = M.min_house(29000000)
-# |> IO.inspect
 
-# ^result = 0
+# Found solution in 51 seconds.
+result = M.min_house(29_000_000)
+|> IO.inspect
+
+^result = 665280
